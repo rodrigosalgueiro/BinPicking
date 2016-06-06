f27bf6755b270ed07e588c958a311966a7469f2f
Miguel Riem de Oliveira <m.riem.oliveira@gmail.com>
2016-06-06 11:50:54 +0100

    adding gitignore

--- ensaio_correspondencia/src/CMakeFiles/allclean.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/allclean"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/allclean.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
